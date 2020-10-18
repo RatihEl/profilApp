f1266dbfbb9ea79d03ded623145f5168d3d52874
RatihEl <ratiheldina1234@gmail.com>
2020-10-18 21:02:20 +0700

    Initial commit

---
+++ app/src/main/java/com/ratih/profileapp/Cofer_profile.kt
@@ -0,0 +1,11 @@
+package com.ratih.profileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cofer_profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cover_profile)
+    }
+}
